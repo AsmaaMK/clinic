49112c5850bac108a0c5e141e2bc508732e4a51f
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2023-12-19 17:49:41 +0200

    Patient profile finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './core/components/header/header.component';
 import { ScrollToTopBtnComponent } from './core/components/scroll-to-top-btn/scroll-to-top-btn.component';
 import { SignUpDoctorComponent } from './pages/sign-up-doctor/sign-up-doctor.component';
 import { ToasterComponent } from './core/components/toaster/toaster.component';
+import { BgComponent } from './app-bg.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { ToasterComponent } from './core/components/toaster/toaster.component';
     ScrollToTopBtnComponent,
     SignUpDoctorComponent,
     ToasterComponent,
+    BgComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- src/app/pages/patient-profile/patient-info/patient-info.component.ts
+++ src/app/pages/patient-profile/patient-info/patient-info.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { Patient } from '../../../core/models/patient.model';
+import { AuthService } from '../../../core/services/auth.service';
+import { PatientsService } from '../../../core/services/patients.service';
+import { Router, RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { ConfirmPopupComponent } from '../../../core/components/confirm-popup/confirm-popup.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-patient-info',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, TranslateModule, ConfirmPopupComponent, DatePipe],
   templateUrl: './patient-info.component.html',
-  styleUrl: './patient-info.component.scss'
+  styleUrl: './patient-info.component.scss',
 })
 export class PatientInfoComponent {
+  patientInfo!: Patient;
+  loading = false;
+  showLogoutModal = false;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private patientService: PatientsService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPatientInfo();
+  }
+
+  getPatientInfo() {
+    this.loading = true;
+    this.patientService.getLoggedInPatientData().subscribe((res) => {
+      this.loading = false;
+      this.patientInfo = res;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.showLogoutModal = false;
+    this.router.navigate(['/']);
+  }
 }
